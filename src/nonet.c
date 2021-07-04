f03fbdb7439ac12eafc3d5a28d6ed0f86f4b9a5c
Michael Speer <knomenet@gmail.com>
2021-07-04 12:15:13 -0400

    remove PATH attack vulnerability, don't use system, refactor
    Executing the previous version of nonet was vulnerable to a path attack when using the --lo option.
    
      PATH="/evil/:$PATH" nonet --lo echo hello
    
    This would have executed 'ip' as root from /evil/ rather than /sbin/
    
    I changed it to use an absolute path, and avoided 'system' since it shells out to execute the given command.
    
    This meant manually fork+exec'ing and all the additional complexity that brings.

--- src/nonet.c
+++ src/nonet.c
@@ -13,11 +13,18 @@
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <linux/wait.h>
 
 #define FAIL_INVOCATION_ERROR 125
 #define FAIL_CANNOT_EXECUTE   126
 #define FAIL_NOT_FOUND        127
 #define FAIL_LO_UP_ERROR      128
+
+int check_and_pop_lo_arg( int *, char *** );
+void run_net_lo_up( void );
+void wait_for_net_lo( int );
+void run_net_lo( void );
 
 int
 main(
@@ -40,42 +47,8 @@ main(
   
   int uid = getuid();
   
-  if( argc > 1 ){
-    if(
-      argv[0] &&
-      argv[1] &&
-      argv[1][0] == '-' &&
-      argv[1][1] == '-' &&
-      argv[1][2] == 'l' &&
-      argv[1][3] == 'o'
-    ){
-      // be root to run ip link set dev lo up as root
-      setuid( 0 );
-      
-      int rv = system( "ip link set dev lo up" );
-      if( rv == -1 ){
-        fprintf(
-          stderr ,
-          "nonet error : error launching or waiting on child process while bringin up lo\n"
-        );
-        
-        return FAIL_LO_UP_ERROR ;
-      }
-      
-      int es = WEXITSTATUS( rv );
-      
-      if( es != 0 ){
-        fprintf(
-          stderr ,
-          "nonet error : failed to 'ip link set dev lo up': %d\n" ,
-          es
-        );
-        
-        return FAIL_LO_UP_ERROR ;
-      }
-      
-      argv ++ ;
-    }
+  if( check_and_pop_lo_arg( &argc, &argv ) ){
+      run_net_lo_up();
   }
   
   // drop permissions
@@ -109,3 +82,149 @@ main(
   
   return 0 ;
 }
+
+int
+check_and_pop_lo_arg(
+  int *    pargc ,
+  char *** pargv
+){
+  char ** argv = * pargv ;
+  
+  // check that arg 0 ( program name ) is set before stepping to arg 1
+  if( ! argv[0] ){
+    return 0 ;
+  }
+  
+  // if there is no arg 1, it can't be --lo
+  if( ! argv[1] ){
+    return 0 ;
+  }
+  
+  // determine if arg 1 is not --lo
+  // if it's not --lo but starts with -, fail with an unknown parameter issue
+  if( strcmp( argv[1], "--lo" ) != 0 ){
+    if( argv[1][0] == '-' ){
+      fprintf( stderr, "nonet error: unknown option: %s\n", argv[1] );
+      exit( FAIL_INVOCATION_ERROR );
+    }
+    
+    // if it doesn't start with -, it's just the program to run, just return that there's no --lo
+    return 0;
+  }
+  
+  // make sure there are no more arguments 
+  if( argv[2] ){
+    if( argv[2][0] == '-' ){
+      fprintf( stderr, "nonet error: unexpected second option: %s\n", argv[2] );
+      exit( FAIL_INVOCATION_ERROR );
+    }
+  }
+  
+  // there is a --lo, "pop" it before returning so
+  
+  (*pargc)-- ;
+  (*pargv)++ ;
+  
+  return 1 ;
+}
+
+void
+run_net_lo_up(
+  void
+){
+  int rv = fork();
+  
+  if( rv < 0 ){
+    fprintf( stderr, "failed to invoke '/sbin/ip link set dev lo up': %s\n", strerror( errno ) );
+    exit( FAIL_LO_UP_ERROR );
+  }
+  
+  if( rv == 0 ){
+    // child process
+    run_net_lo();
+  }
+  
+  if( rv > 0 ){
+    // parent process
+    wait_for_net_lo( rv );
+  }
+  
+  return ;
+}
+
+void
+wait_for_net_lo(
+  int pid
+){
+ retry:;
+  int status = 0 ;
+  int rv = waitpid( pid, &status, 0 );
+  if( rv < 0 ){
+    if( errno == EINTR ){
+      goto retry;
+    }
+    
+    fprintf( stderr, "nonet error: lost --lo fork child?: %s\n", strerror( errno ) );
+    exit( FAIL_LO_UP_ERROR );
+  }
+  
+  if( rv != pid ){
+    fprintf( stderr, "nonet error: impossible error, got wrong child?\n" );
+    exit( FAIL_LO_UP_ERROR );
+  }
+  
+  if( ! WIFEXITED( status ) ){
+    // we got some other wait signal, even possible?
+    goto retry;
+  }
+  
+  if( WEXITSTATUS( status ) != 0 ){
+    // ip command failed
+    fprintf( stderr, "nonet error: ip command failed\n" );
+    exit( FAIL_LO_UP_ERROR );
+  }
+  
+  // it ran successfully, everything is good
+  return ;
+}
+
+void
+run_net_lo( void ){
+  // this is run in a fork
+  // exec the ip command or exit with failure
+  
+  // be root to run ip link set dev lo up as root
+  // setuid( 0 );
+  
+  // use an absolute path to ip to avoid hijacking
+  char * program = "/sbin/ip" ;
+  
+  // pass program-name + arguments to ip
+  char * programArgs[] = {
+    "ip",
+    "link",
+    "set",
+    "dev",
+    "lo",
+    "up",
+    NULL
+  };
+  
+  // don't pass any envvars to ip
+  char * programEnv[] = {
+    NULL
+  };
+  
+  // don't use system or a "p" variant of exec that does PATH lookups to avoid hijacking
+  
+  int rv = execve( program, programArgs, programEnv );
+  
+  if( rv == -1 ){
+    fprintf(
+      stderr ,
+      "nonet error : error launching or waiting on child process while bringin up lo\n"
+    );
+    
+    exit( FAIL_LO_UP_ERROR );
+  }
+}  
